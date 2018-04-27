f88f981da9aff313dea55d2e2f1713337659d4b1
David Tran <davidtran19@yahoo.com>
2018-04-27 14:25:49 +0200

    Started threshold filter

--- Lab1/CPPTest/thresfilter.cpp
+++ Lab1/CPPTest/thresfilter.cpp
@@ -1,6 +1,6 @@
 #include "thresfilter.h"
 
-void thresfilter(const int xsize, const int ysize, pixel* src){
+void thresfilter(const int xsize, const int ysize, unsigned char* src){
 #define uint unsigned int 
 
   uint sum, i, psum, nump;
@@ -8,18 +8,18 @@ void thresfilter(const int xsize, const int ysize, pixel* src){
   nump = xsize * ysize;
 
   for(i = 0, sum = 0; i < nump; i++) {
-    sum += (uint)src[i].r + (uint)src[i].g + (uint)src[i].b;
+    //sum += (uint)src[i].r + (uint)src[i].g + (uint)src[i].b;
   }
 
   sum /= nump;
 
   for(i = 0; i < nump; i++) {
-    psum = (uint)src[i].r + (uint)src[i].g + (uint)src[i].b;
+    //psum = (uint)src[i].r + (uint)src[i].g + (uint)src[i].b;
     if(sum > psum) {
-      src[i].r = src[i].g = src[i].b = 0;
+      //src[i].r = src[i].g = src[i].b = 0;
     }
     else {
-      src[i].r = src[i].g = src[i].b = 255;
+      //src[i].r = src[i].g = src[i].b = 255;
     }
   }
 }
--- Lab1/CPPTest/thresmain.cpp
+++ Lab1/CPPTest/thresmain.cpp
@@ -4,45 +4,89 @@
 #include <ctime>
 #include "ppmio.h"
 #include "thresfilter.h"
+#include <mpi.h>
+#include <iostream>
 
 int main (int argc, char ** argv) {
+    MPI_Init(nullptr, nullptr);
+    int rank{}, world{}, root{0};
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world);
+
     int xsize, ysize, colmax;
-    auto * src = new pixel[MAX_PIXELS];
+    unsigned char* src, *newsrc;
     struct timespec stime{}, etime{};
 
-    /* Take care of the arguments */
+    if(rank == root) {
+        src = new unsigned char[MAX_PIXELS*3];
+        newsrc = new unsigned char[MAX_PIXELS * 3];
+        /* Take care of the arguments */
 
-    if (argc != 3) {
-	fprintf(stderr, "Usage: %s infile outfile\n", argv[0]);
-	exit(1);
+        if (argc != 3) {
+            std::cerr << "Usage " << argv[0] << "infile outfile" << std::endl;
+            return 0;
+        }
+
+        /* read file */
+        if (read_ppm(argv[1], &xsize, &ysize, &colmax, (char *) src) != 0)
+            return 0;
+
+        if (colmax > 255) {
+            std::cerr << "Too large maximum color-component value" << std::endl;
+            return 0;
+        }
     }
 
-    /* read file */
-    if(read_ppm (argv[1], &xsize, &ysize, &colmax, (char *) src) != 0)
-        exit(1);
+    std::cout << "Has read the image, calling filter" << std::endl;
 
-    if (colmax > 255) {
-	fprintf(stderr, "Too large maximum color-component value\n");
-	exit(1);
+    /*****************************************************
+    * MPI PHASE                                          *
+    *                                                    *
+    ******************************************************/
+    MPI_Bcast(&xsize, 1, MPI_INT, root, MPI_COMM_WORLD);
+    MPI_Bcast(&ysize, 1, MPI_INT, root, MPI_COMM_WORLD);
+
+    int ysplit = ysize/world;
+    int rest = ysize%world;
+    int give = rest==0 ? 0 : 1;
+    int steal = world - 1 - rest;
+    auto sendcounts = new int[world];
+    auto displace = new int[world];
+    for(int i=0; i<world; i++) {
+        sendcounts[i] = (ysplit+give) * xsize * 3;
+        displace[i] = (ysplit+give+1) * xsize * 3 * i;
     }
 
-    printf("Has read the image, calling filter\n");
+    if(rest != 0)
+        sendcounts[world-1] = (ysplit - steal) * xsize * 3;
+
+    auto dst = new unsigned char[sendcounts[rank]];
+
+    MPI_Scatterv(src, sendcounts, displace, MPI_UNSIGNED_CHAR, dst, sendcounts[root], MPI_UNSIGNED_CHAR, root, MPI_COMM_WORLD);
+
 
     clock_gettime(CLOCK_REALTIME, &stime);
 
-    thresfilter(xsize, ysize, src);
+    //thresfilter(xsize, ysize, src);
 
     clock_gettime(CLOCK_REALTIME, &etime);
+    MPI_Gather(dst, sendcounts[rank], MPI_UNSIGNED_CHAR, newsrc, sendcounts[root], MPI_UNSIGNED_CHAR, root, MPI_COMM_WORLD);
 
-    printf("Filtering took: %g secs\n", (etime.tv_sec  - stime.tv_sec) +
-	   1e-9*(etime.tv_nsec  - stime.tv_nsec)) ;
+    std::cout << "Filtering took: " << (etime.tv_sec  - stime.tv_sec) +
+                                       1e-9*(etime.tv_nsec  - stime.tv_nsec) << std::endl;
 
     /* write result */
+    std::cout << "Writing output file" << std::endl;
     printf("Writing output file\n");
 
     if(write_ppm (argv[2], xsize, ysize, (char *)src) != 0)
       exit(1);
 
-    delete(src);
+    if(rank == root){
+        delete [] src;
+    }
+    delete [] sendcounts;
+    delete [] displace;
     return(0);
 }
