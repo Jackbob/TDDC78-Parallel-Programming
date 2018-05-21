b233b4b3419bf2e00897b6cc54b10ae5127eb62a
David Tran <davidtran19@yahoo.com>
2018-05-21 20:11:29 +0200

    Added mpprun

--- Lab4_particlesim/run.sh
+++ Lab4_particlesim/run.sh
@@ -3,9 +3,9 @@ set -e
 
 for cores in {1,2,4,8,16}; do
     echo "Cores: $cores " >> "lab4_result.txt"
-    salloc -N1 -n$cores ./particlesim $1  >> "lab4_result.txt"
+    salloc -N1 -n$cores mpprun ./particlesim $1  >> "lab4_result.txt"
     echo ""
 done
 
 echo "Cores: 32" >> "lab4_result.txt"
-salloc -N2 -n32 ./particlesim $1 >> "lab4_result.txt"
+salloc -N2 -n32 mpprun ./particlesim $1 >> "lab4_result.txt"
